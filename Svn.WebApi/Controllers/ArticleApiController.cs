11f2b197f165cf399bf9cdd26b5778e237d86d9c
sreelakshmichenchu <sreelakshmi.c@live.com>
2016-04-18 10:41:11 +0530

    Api controller change

--- Svn.WebApi/Controllers/ArticleApiController.cs
+++ Svn.WebApi/Controllers/ArticleApiController.cs
@@ -1,9 +1,11 @@
-﻿using Svn.Service;
+﻿using Svn.Model;
+using Svn.Service;
 using System;
 using System.Web.Http;
 
 namespace Svn.WebApi.Controllers
 {
+    [RoutePrefix("api/ArticleApi")]
     public class ArticleApiController : ApiController
     {
         readonly IArticleService oArticleService;
@@ -13,14 +15,46 @@ namespace Svn.WebApi.Controllers
             oArticleService = service;
         }
 
+        [Route("")]
+        [HttpGet]
         public object Get()
         {
             return oArticleService.GetPublishedItems;
         }
 
-        public object Get(string userId)
+        [Route("User/{userId}")]
+        [HttpGets]
+        public object GetByUserID(string userId)
         {
             return oArticleService.GetItemsByUserID(Guid.Parse(userId));
+        }
+
+        [Route("Article/{articleId}")]
+        [HttpGet]
+        public object GetByArticeID(string articleId)
+        {
+            return oArticleService.GetById(Guid.Parse(articleId));
+        }
+
+        [Route("Article/SaveAsDraft")]
+        [HttpPost]
+        public void SaveAsDraft(Article article)
+        {
+            oArticleService.SaveAsDraft(article);
+        }
+
+        [Route("Article/Submit")]
+        [HttpPost]
+        public void Submit(Article article)
+        {
+            oArticleService.Submit(article);
+        }
+
+        [Route("Article/Delete")]
+        [HttpDelete]
+        public void Delete(Article article)
+        {
+            oArticleService.Delete(article);
         }
     }
 }

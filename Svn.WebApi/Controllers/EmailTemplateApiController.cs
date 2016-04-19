9d7283882e5f595030f38d9ffd1646605cb9c835
sreelakshmichenchu <sreelakshmi.c@live.com>
2016-04-19 09:38:33 +0530

    EmailTemplate api changes

--- Svn.WebApi/Controllers/EmailTemplateApiController.cs
+++ Svn.WebApi/Controllers/EmailTemplateApiController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq;
 using Svn.Service;
 using System.Web.Http;
+using Svn.Model;
 
 namespace Svn.WebApi.Controllers
 {
+    [RoutePrefix("api/EmailTemplateApi")]
     [Authorize]
     public class EmailTemplateApiController : ApiController
     {
@@ -14,14 +16,39 @@ namespace Svn.WebApi.Controllers
             oEmailService = service;
         }
 
+        [Route("")]
+        [HttpGet]
         public object Get()
         {
             return oEmailService.GetAll.Select(s => s.Code);
         }
 
+        [Route("Email/GET")]
+        [HttpGet]
         public object Get(string templateName)
         {
             return oEmailService.GetEmailTemplateByCode(templateName);
+        }
+
+        [Route("Save")]
+        [HttpPost]
+        public void Save(EmailTemplate eTemplate)
+        {
+            oEmailService.Create(eTemplate);
+        }
+
+        [Route("Update")]
+        [HttpPost]
+        public void Update(EmailTemplate eTemplate)
+        {
+            oEmailService.Update(eTemplate);
+        }
+
+        [Route("Delete")]
+        [HttpDelete]
+        public void Delete(EmailTemplate eTemplate)
+        {
+            oEmailService.Delete(eTemplate);
         }
     }
 }
